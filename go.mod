fb65e73f3dee42f3543058e912ec56150119e8d8
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-05 12:39:45 +1100

    Get placename and proverb from database

--- go.mod
+++ go.mod
@@ -3,9 +3,18 @@ module github.com/shanehowearth/proverbs
 go 1.15
 
 require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
 	github.com/dghubble/oauth1 v0.6.0
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
+	golang.org/x/text v0.3.4 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
---
+++ storage/postgres/postgres.go
@@ -0,0 +1,83 @@
+// Package postgresstore -
+package postgresstore
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/jackc/pgx"
+)
+
+type pg struct {
+	dbConn *pgx.ConnPool
+}
+
+// NewPGStore -
+// nolint:golint
+func NewPGStore(host, database, user, password string) (*pg, error) {
+	pgxConfig := pgx.ConnConfig{
+		Host:     host,
+		Database: database,
+		User:     user,
+		Password: password,
+	}
+	pgxConnPoolConfig := pgx.ConnPoolConfig{ConnConfig: pgxConfig, MaxConnections: 3, AfterConnect: nil, AcquireTimeout: 0}
+	conn, err := pgx.NewConnPool(pgxConnPoolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create db connection with error %w", err)
+	}
+	p := pg{
+		dbConn: conn,
+	}
+	return &p, nil
+}
+
+// GetProverb -
+func (pg *pg) GetProverb() (proverb string, translation string, err error) {
+	rowCount, err := pg.getRowCount("proverb")
+	if err != nil {
+		return "", "", fmt.Errorf("unable to get rowcount for proverb table with error %w", err)
+	}
+
+	// select a row
+	id := pg.getRandomID(rowCount)
+	err = pg.dbConn.QueryRow("select  maori, pakeha from proverb where id = $1", id).Scan(&proverb, &translation)
+	if err != nil {
+		return "", "", fmt.Errorf("unable to query proverb with error %w", err)
+	}
+
+	return proverb, translation, nil
+}
+
+// getRowCount -
+func (pg *pg) getRowCount(table string) (int64, error) {
+	var n int64
+	query := fmt.Sprintf("SELECT COUNT(id) FROM %s", pgx.Identifier.Sanitize([]string{table}))
+	err := pg.dbConn.QueryRow(query).Scan(&n)
+	return n, err
+}
+
+// getRandomID -
+func (pg *pg) getRandomID(rowcount int64) int64 {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	return r1.Int63n(rowcount + 1)
+}
+
+// GetPlacename -
+func (pg *pg) GetPlacename() (placename, translation string, err error) {
+	rowCount, err := pg.getRowCount("placename")
+	if err != nil {
+		return "", "", fmt.Errorf("unable to get rowcount for placename table with error %w", err)
+	}
+
+	// select a row
+	id := pg.getRandomID(rowCount)
+	err = pg.dbConn.QueryRow("select  maori, pakeha from placename where id = $1", id).Scan(&placename, &translation)
+	if err != nil {
+		return "", "", fmt.Errorf("unable to query placename with error %w", err)
+	}
+
+	return placename, translation, nil
+}
