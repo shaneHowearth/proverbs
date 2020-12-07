070b18421ac9186cb9064d6cc4e2a3e6c89ef120
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-07 15:51:37 +1100

    Add GetContent function to Store that gets a proverb or placename depending on the time of day

--- storage/store.go
+++ storage/store.go
@@ -1,8 +1,36 @@
 // Package storage -
 package storage
 
+import (
+	"strconv"
+	"time"
+)
+
 // Store -
 type Store interface {
 	GetRandomProverb() (proverb, translation, explanation string, err error)
-	GetRandomPlacename() (proverb, translation, explanation string, err error)
+	GetRandomPlacename() (placename, translation, explanation string, err error)
+}
+
+func isMorning() bool {
+	now := time.Now()
+	newLayout := "15:04"
+	check, _ := time.Parse(newLayout, strconv.Itoa(now.Hour())+":"+strconv.Itoa(now.Minute()))
+	start, _ := time.Parse(newLayout, "23:59")
+	end, _ := time.Parse(newLayout, "11:59")
+	start, end = start.UTC(), end.UTC()
+	if start.After(end) {
+		start, end = end, start
+
+	}
+	check = check.UTC()
+	return !check.Before(start) && !check.After(end)
+
+}
+
+func GetContent(s Store) (content, translation, explanation string, err error) {
+	if isMorning() {
+		return s.GetRandomProverb()
+	}
+	return s.GetRandomPlacename()
 }
