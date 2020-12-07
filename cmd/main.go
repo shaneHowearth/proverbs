4c194fbdd962d94c1de7ed040fd507db69a2027c
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-07 15:52:07 +1100

    Interact with storage in main

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/shanehowearth/proverbs/publish"
 	"github.com/shanehowearth/proverbs/publish/twitter"
+	"github.com/shanehowearth/proverbs/storage"
+	postgresstore "github.com/shanehowearth/proverbs/storage/postgres"
 )
 
 func main() {
@@ -22,7 +25,31 @@ func main() {
 	}
 	platforms := []publish.Publisher{}
 	platforms = append(platforms, tc)
-	// tc.PublishContent("Tēnā koe e hoa")
 	// Connect to the data store
+	pgHost := os.Getenv("POSTGRES_HOST")
+	pgDB := os.Getenv("POSTGRES_DATABASE")
+	pgUser := os.Getenv("POSTGRES_USER")
+	pgPW := os.Getenv("POSTGRES_PASSWORD")
+	db, err := postgresstore.NewPGStore(pgHost, pgDB, pgUser, pgPW)
+	if err != nil {
+		log.Fatalf("Unable to create postgres connection with error %v", err)
+	}
+
+	// Retrieve content
+	data, translation, explanation, err := storage.GetContent(db)
+	if err != nil {
+		log.Fatalf("Unable to retrieve content with error %v", err)
+	}
+
+	content := strings.Join([]string{data, translation, explanation}, "\n\n")
+	// Publish content
+	for idx := range platforms {
+		err := platforms[idx].PublishContent(content)
+		if err != nil {
+			// not the end of the world so don't die, but someone somewhere
+			// should have a look at it
+			log.Printf("ERROR: Getting content for %v returned error %v", platforms[idx], err)
+		}
+	}
 
 }
