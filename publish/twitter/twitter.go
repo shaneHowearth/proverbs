31876738fc883ee8d87fe40b686bb055b2ce6cb2
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-07 14:47:12 +1100

    If the supplied content is longer than a tweet, then break it up and create a thread

--- publish/twitter/twitter.go
+++ publish/twitter/twitter.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"unicode/utf8"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
@@ -30,13 +31,31 @@ func NewTwitterClient(consumerKey, consumerSecret, accessToken, accessSecret str
 	return &twitterClient{twitter.NewClient(httpClient)}, nil
 }
 
-func (t *twitterClient) PublishContent(s string) error {
-	_, resp, err := t.Statuses.Update(s, nil)
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("http return status was %d, with %s", resp.StatusCode, resp.Status)
-		log.Printf("accompanied with error: %v", err)
-		err = fmt.Errorf("bad status code returned with error %w", err)
+func (t *twitterClient) PublishContent(content string) error {
+	bottom := 0
+	top := 280
+	l := utf8.RuneCountInString(content)
+	params := &twitter.StatusUpdateParams{}
+	for bottom < l {
+		snippet := []rune(content)[bottom:top]
+		for {
+			if snippet[top] != ' ' {
+				top--
+			} else {
+				break
+			}
+		}
+		tweet, resp, err := t.Statuses.Update(string(snippet[bottom:top]), params)
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("http return status was %d, with %s", resp.StatusCode, resp.Status)
+			log.Printf("accompanied with error: %v", err)
+			err = fmt.Errorf("bad status code returned with error %w", err)
+			return err
+		}
+		params.InReplyToStatusID = tweet.ID
+		bottom = top
+		top += 280
 	}
-	return err
+	return nil
 
 }
