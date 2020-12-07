b912aa560422840777e578ac6d36d2f4c465e206
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-07 18:08:18 +1100

    Begin adding proverbs

---
+++ storage/postgres/data/20201207130707_insert_proverbs.sql
@@ -0,0 +1,94 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+INSERT INTO proverbs (maori_name, translation) VALUES('He ringa miti tai heke.', 'A hand which licks up the ebb tide.');
+INSERT INTO proverbs (maori_name, translation) VALUES('E rua tau ruru,
+E rua tau wehe
+E rua tau mutu,
+E rua tau kai.', 'Two years of wind and storm,
+Two years when food is scarce,
+Two years when crops fail,
+Two years of abundant food.
+');
+INSERT INTO proverbs (maori_name, translation) VALUES('Hinana ki uta, Hinana ki tai.', 'Hinana to the interior, Hinana to the sea.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ko te kai a Māui, he ringaringa kau tāhanga.', 'The food of Māui, an empty hand.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He whatitiri ki te rangi, ko Te Arawa ki te whenua.', 'As thunder in the sky, so is the Arawa tribe on earth.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He ao te rangi ka ūhia, he huruhuru te manu ka tau.', 'As the clouds cover the sky, so the feathers cover the bird.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ko te wahine mā tētehi, ko te whare mō tētehi.', 'The woman for one, the house for the other.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Honoa te hono a te kiore.', 'Add to the company of rats.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Hinga iho, tomo atu i te pā.', 'With the strength of the enemy overcome, be quick to attack the pā.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Kia mau koe ki te kupu a tōu matua.', 'Hold fast to the words your father gives you.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Whāngai i tō tāua tuahine, hei tangi i a tāua.', 'Let us nurture our sister and she will mourn for us.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Te kuku o te manawa.', 'The pincers of the heart.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka eke anō i te puke Ruahine.', 'He is climbing the mountain of Ruahine.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Tēnā ko te toa mahi kai e kore e paheke.', 'A warrior who works hard at growing food will not fail.');
+INSERT INTO proverbs (maori_name, translation) VALUES('E moe ana te mata hī tuna, e ara ana te mata hī aua.', 'When the eyes of those who fish for eels are sleeping, the eyes of those who catch mullet are open.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka whakarongo pīkari ngā taringa.', 'The ears listening like fledglings.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He rei ngā niho, he parāoa ngā kauae.', E'A whale\'s tooth in a whale\'s jaw.
+(If you have the tooth of a whale, you must have the jaw of a whale to hold it.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He whetū ka haere ki te kai i te marama, ki te taha ki tōna hoa riri e kore rā ia e toa.', E'A star sets off to devour the moon, but as far as its enemy (the moon) is concerned, the star will never succeed.
+One should realise one\'s limits.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('E tū i te tū a Tāne-rore, e haka i te haka a Tāne-rore. Kaua i te tū, i te haka a te keretao.', 'Adopt the stance of Tāne-rore, and perform as Tāne-rore does. Do not perform as a puppet does.
+(Advice to the men in the haka to be alive and display masculine grace, rather than be regimented, limp and wooden like a puppet.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He kākā tino tangata.', E'The ugly offspring.
+(Appearances may be deceptive. Though they may be ugly and unattractive, they come from a line of great chiefs. The expression has the brevity and meaning of \'ugly duckling\')');
+INSERT INTO proverbs (maori_name, translation) VALUES('Te iti oneone i kapunga mai i Hawaiki.', E'A little bit of earth from Hawaiki in the hollow of the hand.
+(A proverb used of appetising food secured at great cost. - It\'s a reference to the Kumara and other plants carried across the seas to Aotearoa from Hawaiki)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Tēnā te kakara o Tutunui', 'That is the savoury smell of Tutunui.
+(Tutunui was a whale belonging to the chief Tinirau, who lent it to the tohunga Kae to convey him to his home. Kae killed the whale and his people cooked it, covering the flesh with leaves of the koromiko shrub. Tinirau looked in vain for the return of his whale, but when he smelled the roasting meat he knew what had happened and took revenge on Kae. When koromiko branches are thrown on the fire they give out a distinctive odour, which gave rise to this saying.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Iro te iro, homai kia kainga, ka kai hoki ia i a au.', 'Flyblown or not, give it here to me; I will eat the maggots now, for eventually.
+(The saying was quoted to one who was over-particular about his food.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Kino atu, ki te aro aro o Taiawa.', 'If it is very bad bring it to Taiawa.
+(Taiawa was noted for his insatiable appetite.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Kia whakarongo pīkari aku taringa.', 'My ears hear the nestlings.
+(Like nestlings eagerly awaiting the parent bird.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He aha koa. Hai te tokorima a Māui.', 'It does not matter. I have the five of Māui.
+(If the host apologises for the lack of cutlery available, the guest replies that he has his fingers - the five of Māui.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ko te moa kai hau.', 'Like the moa eating the wind.
+(If you can live without food then you are like the moa, which always stood on one leg with its beak open, living on the wind.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He moa oti koe, inā ye kore koe e kai.', '');
+INSERT INTO proverbs (maori_name, translation) VALUES('Tāu mahi e te ringa whero.', 'Fit work for the hand of a chief.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Kia kino te tahā, kia tū noa ai i te marae.', 'An unornamented calabash may safely be left outside.
+(Similarly a plain-looking wife is safe from the attentions of other men.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Aitia te wahine i roto i te pā harakeke.', 'Marry the woman in the flaxbush.
+(If a woman is frequently seen gathering flax, it is an indication that she is a weaver. As such she is eminently eligible and should be preferred as a spouse.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He kahawai ki te wai, he wahine ki uta.', 'A kahawai in the water, a woman on land.
+(The capriciousness of women, or the unpredictability of men in choosing their wives. The kahawai is noted for taking only bait which resembled its normal food. Similarly, a woman is difficult to please, especially when it comes to choosing a man.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Haere atu te wahine, haere maro kore.', E'The woman goes, and goes without her kilt.
+(If a woman marries a man of another tribe she forfeits all rights to land in her mother\'s tribe.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He pai tāne e koro e reia, he kino wahine ka reia.', 'A handsome man will not after, but a plain woman will be run after eagerly.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Nā tō tamāhine pai i tākina mai ai tēnei kekeno ki konei.', 'It was this lovely girl who brought the seal here.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He kaka tīhorea he tohu nō te wahine mahue.', 'A discarded garment is the sign of a deserted woman.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He puta taua ki te tāne, whānau tamariki ki te wahine.', 'The battlefield for man, childbirth for woman.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He kupu matangerengere.', 'A disagreeable word.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka roa te ngaromanga, he iti te putanga.', 'When it is hidden for a long time, it will be smalle when it comes forth.
+(A man who has a great deal to say, but who does not put it into action.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ihu oneone.', 'Good worker.
+(Ihu means nose, oneone means soil. Literally dirty nose, and indicates a person who is working hard and bears the marks of toil, equivalent to Nose to the grindstone.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Kia noho i taku kōtere, kia ngenge te pakihiwi.', 'Sometimes sitting on your buttocks, sometimes exhausted by the work of your shoulders.
+(Applied to paddlers in a canoe.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Pai tū, pai hinga, nā wai, nā oti.', 'Good at standing, good at falling, the work is eventually finished.
+(The thought is that whether it be well done, or badly, eventually it comes to an end.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Takato kau ana te whānau a Tāne.', 'The children of Tāne lie prone.
+(A saying indicating that once the trees are cleared away, the work is done.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Anō me he whare pūngawerewere.', E'As if it were a spider\'s web.
+(A saying applied to fine and intricate work in carving or weaving.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He rā whatiwhati kō.', 'A day for breaking the spade.
+(An expression used for a day when everything is right for hard work. The kō was a long-handled digging implement.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Tēnā te ringa tango parahia.', 'This is the hand which pulls out the weeds.
+(Applied to a hard worker.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka mahi te hukuroa i ana mahi.', 'Well done! The band of toilers keeps on working.');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ngā uri o Rongomai toki kino.', 'The descendants of Rongomai, who handle the adze badly.');
+INSERT INTO proverbs (maori_name, translation) VALUES('He manu aute e taea te whakahoro.', 'A kite which, when slackened off, flies away.
+(A lover compares himself to a kite and longs to be released in order that he may fly to his loved one.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('He pōtiki whakahirahira.', 'The last-born with a good opinion of himself.
+(The youngest son was often spoilt, ambitious, and self-opinionated)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka pū te rūhā, ka hao te rangatahi.', 'The old rags lie in a heap, while the net is used for fishing.
+(When the old net is worn out and cast aside, the new net is put into use.)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Ka haere te tōtara haemata, ka takoto te pukatea wai nui.', 'The tōtara chips float while the pukatea lies in deep water.
+(Young people may travel, while older people are forced to stay at home)');
+INSERT INTO proverbs (maori_name, translation) VALUES('Mahia ngā mahi kei tamariki ana.', 'Make the most of your time when you are young.');
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+/* DELETE FROM proverbs WHERE maori_name='Amuri' AND translation=''; */
+/* DELETE FROM proverbs WHERE maori_name='Amuri' AND translation=''; */
