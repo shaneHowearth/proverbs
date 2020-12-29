d487da03bda7c6d902cf7de8a6952d2e97a7410c
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-29 13:17:42 +1100

    Added all the i placenames

---
+++ storage/postgres/data/20201229112308_proverbs_start_with_i.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Ihumātao', 'ihu (nose); mātao (cold).', 'Cold nose.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Ihu-ngau-ana', 'ihu (nose); ngau (bite); ana (verb particle).', 'Nose bitten off. The chief Tamahaki had his nose bitten off by Kura at a battle here on the Whanganui River.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Ihumoana', 'ihu (nose); moana (sea).', 'Headland projecting into the sea.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Ikamatua', 'ika (fish); matua (parent, fully grown adult).', 'One explanation is that it is the shortened form of Te Ika-a-matua, The fish of my ancestor (Māui).');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Ikoraki', 'The South Island version of Hikurangi.', '');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Inangahua', 'inanga (whitebait); hua (preserved by drying in the sun, or plenty of).', 'The Inangahua River was known for big catches of whitebait.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Irimahuwheri', 'The correct name of this headland in the Buller district is Irimahuwhero. Iri (hanging); mahu (perhaps a contraction of māhunga, hair); whero (red).', 'Hanging red hair. On the seaward side of the headland there are masses of rātā trees, which are sometimes ablaze with red blooms.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Iwikatea', 'iwi (bone); katea (bleached).', 'Bleached bones. This is the Māori name for the site of Balclutha. A great battle took place here long ago, and the bones remained unburied for many years.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Iwirua', 'iwi (bone); rua (pit, or cave).', 'There may have been a burial ground on this cape in Grove Arm, Marlborough Sounds.');
+INSERT INTO placenames (maori_name, translation, explanation) VALUES('Iwituaroa', 'Backbone.', 'From the sea to the Iwituaroa Range bears a striking resemblance to a human backbone.');
